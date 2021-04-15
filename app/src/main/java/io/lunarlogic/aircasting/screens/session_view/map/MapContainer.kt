090b40c79a8ef21738104d8ec6b2da5f9550fa0b
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2021-04-15 14:48:58 +0200

    very rough TileOverlay

--- app/src/main/java/io/lunarlogic/aircasting/screens/session_view/map/MapContainer.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/session_view/map/MapContainer.kt
@@ -21,8 +21,12 @@ import io.lunarlogic.aircasting.models.MeasurementStream
 import io.lunarlogic.aircasting.models.Session
 import io.lunarlogic.aircasting.screens.session_view.SessionDetailsViewMvc
 import kotlinx.android.synthetic.main.activity_map.view.*
+import java.lang.Math.*
+import java.net.MalformedURLException
+import java.net.URL
 import java.util.ArrayList
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.math.asinh
 
 class MapContainer: OnMapReadyCallback {
     private val DEFAULT_ZOOM = 16f
@@ -90,6 +94,7 @@ class MapContainer: OnMapReadyCallback {
         mMap?.isBuildingsEnabled = false
 
         drawSession()
+
         animateCameraToSession()
         if (mMeasurements.isNotEmpty()) showMap()
     }
@@ -137,6 +142,11 @@ class MapContainer: OnMapReadyCallback {
         if (latestPoint != null && latestColor != null) {
             drawLastMeasurementMarker(latestPoint, latestColor)
         }
+
+        val tileOverlay = mMap?.addTileOverlay(
+            TileOverlayOptions()
+                .tileProvider(getTileProvider()).visible(true)
+        )
     }
 
     private fun drawLastMeasurementMarker(point: LatLng?, color: Int?) {
@@ -255,6 +265,62 @@ class MapContainer: OnMapReadyCallback {
     private fun showMap() {
         mMapFragment?.view?.visibility = View.VISIBLE
         mLocateButton?.visibility = View.VISIBLE
+    }
+
+    //TILES
+    private fun getTileProvider(): TileProvider {
+        var tileProvider: TileProvider = object : UrlTileProvider(256, 256) {
+
+            override fun getTileUrl(x: Int, y: Int, zoom: Int): URL? {
+                println("MARYSIA: tile ${x}x${y}")
+                /* Define the URL pattern for the tile images */
+                val url = "https://www.iconsdb.com/icons/preview/green/square-xxl.png"
+                return if (!checkTileExists(x, y, zoom)) {
+                    null
+                } else try {
+                    URL(url)
+                } catch (e: MalformedURLException) {
+                    throw AssertionError(e)
+                }
+            }
+
+            /*
+         * Check that the tile server supports the requested x, y and zoom.
+         * Complete this stub according to the tile range you support.
+         * If you support a limited range of tiles at different zoom levels, then you
+         * need to define the supported x, y range at each zoom level.
+         */
+            private fun checkTileExists(x: Int, y: Int, zoom: Int): Boolean {
+                println("MARYSIA: zoom: ${zoom}")
+                val minZoom = 1
+                val maxZoom = 20
+                return zoom in minZoom..maxZoom
+            }
+
+
+        }
+        return tileProvider
+    }
+
+    private fun getXYTile(lat : Double, lon: Double, zoom : Int) : Pair<Int, Int> {
+        val latRad = Math.toRadians(lat)
+        var xtile = floor( (lon + 180) / 360 * (1 shl zoom) ).toInt()
+        var ytile = floor( (1.0 - asinh(tan(latRad)) / PI) / 2 * (1 shl zoom) ).toInt()
+
+        if (xtile < 0) {
+            xtile = 0
+        }
+        if (xtile >= (1 shl zoom)) {
+            xtile= (1 shl zoom) - 1
+        }
+        if (ytile < 0) {
+            ytile = 0
+        }
+        if (ytile >= (1 shl zoom)) {
+            ytile = (1 shl zoom) - 1
+        }
+
+        return Pair(xtile, ytile)
     }
 }
 
